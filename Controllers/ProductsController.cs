d14b3ddbdf29d5337771d5475cbc81f1b40889cf
Sami Palomäki <sami.palomaki@hotmail.com>
2021-12-15 20:32:51 +0200

    Add files via upload

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppFirst.Models;
+
+namespace WebAppFirst.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            if (Session["UserName"] == null)
+            {
+                return RedirectToAction("login", "home");
+            }
+            else
+            {
+                NorthwindEntities db = new NorthwindEntities();
+                List<Products> model = db.Products.ToList();
+                db.Dispose();
+                return View(model);
+            }
+        }
+
+        public ActionResult Index2()
+        {
+            if (Session["UserName"] == null)
+            {
+                return RedirectToAction("login", "home");
+            }
+            else
+            {
+                NorthwindEntities db = new NorthwindEntities();
+                List<Products> model = db.Products.ToList();
+                db.Dispose();
+                return View(model);
+            }
+        }
+    }
+}
---
+++ Controllers/ShippersController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebAppFirst.Models;
+
+namespace WebAppFirst.Controllers
+{
+    public class ShippersController : Controller
+    {
+        // GET: Shippers
+        NorthwindEntities db = new NorthwindEntities();
+        public ActionResult Index()
+        {
+            if (Session["UserName"] == null)
+            {
+                return RedirectToAction("login", "home");
+            }
+            else
+            {
+                var shippers = db.Shippers.Include(s => s.Region);
+                return View(shippers.ToList());
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)                   // Linkkipyyntö edittiin
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Shippers shippers = db.Shippers.Find(id);
+            if (shippers == null) return HttpNotFound();    // Jos ei löydy, palautetaan HttpNotFound
+            ViewBag.RegionID = new SelectList(db.Region, "RegionID", "RegionDescription", shippers.RegionID);
+            return View(shippers);                          // Jos löytyy palautetaan näkymä
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] //Katso https://go.microsoft.com/fwlink/?LinkId=317598
+        public ActionResult Edit([Bind(Include = "ShipperID,CompanyName,Phone,RegionID")] Shippers shipper)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(shipper).State = EntityState.Modified;
+                db.SaveChanges();
+                ViewBag.RegionID = new SelectList(db.Region, "RegionID", "RegionDescription", shipper.RegionID);
+                return RedirectToAction("Index");
+            }
+            return View(shipper);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.RegionID = new SelectList(db.Region, "RegionID", "RegionDescription");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ShipperID,CompanyName,Phone,RegionID")] Shippers shipper)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Shippers.Add(shipper);
+                db.SaveChanges();
+                ViewBag.RegionID = new SelectList(db.Region, "RegionID", "RegionDescription");
+                return RedirectToAction("Index");
+            }
+            return View(shipper);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            Shippers shippers = db.Shippers.Find(id);
+            if (shippers == null) return HttpNotFound();
+            return View(shippers);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Shippers shippers = db.Shippers.Find(id);
+            db.Shippers.Remove(shippers);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
